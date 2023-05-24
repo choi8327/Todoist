f779a339f33e5a3db8a09f860132fcd4c2fb9101
choi8327 <choi8327@gmail.com>
2023-05-24 10:23:28 +0900

    first commit

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" @click="clearTodo">모두 지우기</span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearTodo() {
+      this.$emit('removeAll');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+    float: center;
+  }
+  .clearAllBtn {
+    color: #e20303;
+		/* 추가 */
+		display: block;
+  }
+</style>
